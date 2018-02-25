9c3679c901ed23c149729d183379caa655d9bb45
Rohan <rhn.vma@gmail.com>
2018-02-25 05:40:03 +0530

    fix the files

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,38 +1,40 @@
-// SOCKET EVENT HANDLERS
-
 var socket = io();
 
 function scrollToBottom () {
   // Selectors
   var messages = jQuery('#messages');
-  var newMessage = jQuery('#messages li').last();
-  var lastMessage = jQuery('#messages li:nth-last-child(2)');
+  var newMessage = messages.children('li:last-child')
   // Heights
   var clientHeight = messages.prop('clientHeight');
   var scrollTop = messages.prop('scrollTop');
   var scrollHeight = messages.prop('scrollHeight');
-  var newMessageHeight =  newMessage.height();
-  var lastMessageHeight = lastMessage.height();
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
 
   if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
     messages.scrollTop(scrollHeight);
   }
-
 }
 
-socket.on('connect', function() {
-  console.log('Connected to server');
+socket.on('connect', function () {
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function (err) {
+    if (err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No error');
+    }
+  });
 });
 
-socket.on('disconnect', function() {
+socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-
-socket.on('newMessage', function(message) {
-
+socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-
   var template = jQuery('#message-template').html();
   var html = Mustache.render(template, {
     text: message.text,
@@ -42,17 +44,11 @@ socket.on('newMessage', function(message) {
 
   jQuery('#messages').append(html);
   scrollToBottom();
-
-
 });
 
-
-
-socket.on('newLocationMessage', function(message) {
-
+socket.on('newLocationMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-
-  var template = jQuery("#location-message-template").html();
+  var template = jQuery('#location-message-template').html();
   var html = Mustache.render(template, {
     from: message.from,
     url: message.url,
@@ -61,14 +57,9 @@ socket.on('newLocationMessage', function(message) {
 
   jQuery('#messages').append(html);
   scrollToBottom();
-
 });
 
-
-// SEND MESSAGE BUTTON EVENT HANDLER
-
-
-jQuery('#message-form').on('submit', function(e) {
+jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
   var messageTextbox = jQuery('[name=message]');
@@ -76,34 +67,27 @@ jQuery('#message-form').on('submit', function(e) {
   socket.emit('createMessage', {
     from: 'User',
     text: messageTextbox.val()
-
-  }, function() {
+  }, function () {
     messageTextbox.val('')
   });
-
 });
 
-
-// LOCATION BUTTON EVENT HANDLER
-
 var locationButton = jQuery('#send-location');
-
-
-locationButton.on('click', function() {
+locationButton.on('click', function () {
   if (!navigator.geolocation) {
-    return alert('Geolocation not supported by your browser');
+    return alert('Geolocation not supported by your browser.');
   }
 
-  locationButton.attr('disabled', 'disabled');
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
-  navigator.geolocation.getCurrentPosition(function(position) {
-    locationButton.removeAttr('disabled');
+  navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
-    })
-  }, function(e) {
-    locationButton.removeAttr('disabled');
-    console.log(e);;
-  });;
-})
+    });
+  }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
+    alert('Unable to fetch location.');
+  });
+});
--- server/utils/message.js
+++ server/utils/message.js
@@ -8,13 +8,12 @@ var generateMessage = (from, text) => {
   };
 };
 
-var generateLocationMessage = (from,latitude,longitude) => {
+var generateLocationMessage = (from, latitude, longitude) => {
   return {
     from,
-    url:`https://www.google.com/maps?q=${latitude},${longitude}`,
+    url: `https://www.google.com/maps?q=${latitude},${longitude}`,
     createdAt: moment().valueOf()
   };
 };
-
 
 module.exports = {generateMessage, generateLocationMessage};
